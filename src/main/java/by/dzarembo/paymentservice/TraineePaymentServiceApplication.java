bb69b2989edda2ca0aad427624f9e782f98802db
dzarembo <dima.zarembo@gmail.com>
2026-04-15 16:32:33 +0300

    init commit

---
+++ src/main/java/by/dzarembo/paymentservice/TraineePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package by.dzarembo.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraineePaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/dzarembo/paymentservice/TraineePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dzarembo.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineePaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
